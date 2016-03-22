acfe6e0d668182a16ece46fefbf4008c7d126701
Emily Uderman <emily.uderman@hamonusa.com>
2016-03-22 16:38:49 +0000

    almost time to go live! commit some theme updates

--- themes/custom/bluetheme/fz/js/app.js
+++ themes/custom/bluetheme/fz/js/app.js
@@ -1,4 +1,33 @@
 
+
+// $('.big-slides').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 3,
+//   variableWidth: true
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
+
+
 // /*!
 //  * jQuery Sticky Footer 1.1
 //  * Corey Snyder
