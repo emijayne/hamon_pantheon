63b3b9d03469a49db23508f06c02247f5a8c6a37
emijayne <emijayne@gmail.com>
2016-08-23 13:33:48 -0400

    adding multiple maps and a necessary semicolon

--- themes/custom/bluetheme/fz/js/map.js
+++ themes/custom/bluetheme/fz/js/map.js
@@ -1,9 +1,5 @@
 (function ($) {
 
-  // map tile
-  var basemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy Hamon', maxZoom: 16, accessToken: 'pk.eyJ1IjoiZW1pamF5bmUiLCJhIjoiY2lvMWxrbGxrMWFtaXVrbTNmNWE2dWZkZyJ9.1mt_VncCBhOtJSpSsXN96g'})
-
   // maps
   var mapmain = L.map('locationmap').setView([44.9925, -93.449522], 4);
   var maphc = L.map('hcoffices').setView([44.9925, -93.449522], 4);
@@ -11,6 +7,12 @@
   var maphrcreps = L.map('hrcrepmap').setView([44.9925, -93.449522], 4);
   var maphdireps = L.map('hdirepmap').setView([44.9925, -93.449522], 4);
   var maprccreps = L.map('rccrepmap').setView([44.9925, -93.449522], 4);
+
+  // map tile
+  var basemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy Hamon', 
+    maxZoom: 16, 
+    accessToken: 'pk.eyJ1IjoiZW1pamF5bmUiLCJhIjoiY2lvMWxrbGxrMWFtaXVrbTNmNWE2dWZkZyJ9.1mt_VncCBhOtJSpSsXN96g'});
 
   // offices
   var locCorp = L.marker([40.567, -74.609997]);
@@ -24,12 +26,12 @@
   // www.hamonusa.com/locations
   basemap.addTo(mapmain);
   locCorp.addTo(mapmain).openPopup()
-    .bindPopup("<b>Hamon Corporate Headquarters</b><br />Hamon Custodis, Inc. | <em>Corp &amp; Eastern Office</em><br />Hamon Research Cottrell, Inc.<br />Research Cottrell Cooling, Inc. | <em>Corp &amp; Eastern Office</em>")    ;
+    .bindPopup("<b>Hamon Corporate Headquarters</b><br />Hamon Custodis, Inc. | <em>Corp &amp; Eastern Office</em><br />Hamon Research Cottrell, Inc.<br />Research Cottrell Cooling, Inc. | <em>Corp &amp; Eastern Office</em>");
   lochcal.addTo(mapmain).bindPopup("<em><strong>Southern Office</strong></em><br />Hamon Custodis, Inc.<br />Research Cottrell Cooling, Inc.");
   lochcin.addTo(mapmain).bindPopup("<em><strong>MidWest Office</strong></em><br />Hamon Custodis, Inc.<br />Research Cottrell Cooling, Inc.");
   lochcut.addTo(mapmain).bindPopup("<em><strong>Western Office</strong></em><br />Hamon Custodis, Inc.<br />Research Cottrell Cooling, Inc.");
-  lochdi.addTo(mapmain);
   lochcca.addTo(mapmain);
+  lochdi.addTo(mapmain);
   locttc.addTo(mapmain);
 
   // www.hamonusa.com/locations/hc
@@ -40,7 +42,7 @@
   lochcut.addTo(maphc).bindPopup("Western Office");
   lochcca.addTo(maphc);
 
-  // www.hamonusa.com/locations/hc
+  // www.hamonusa.com/locations/rcc
   basemap.addTo(maprcc);
   locCorp.addTo(maprcc).bindPopup("Eastern Region Office");
   lochcal.addTo(maprcc).bindPopup("Southern Office");
