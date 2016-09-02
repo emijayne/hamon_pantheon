d7a21ac346320504abfdd642108b2c59c385e6c8
emijayne <emijayne@gmail.com>
2016-09-02 13:01:25 -0400

    ok, maybe we will separate files instead

--- themes/custom/bluetheme/fz/js/maphc.js
+++ themes/custom/bluetheme/fz/js/maphc.js
@@ -5,23 +5,16 @@ var txt_hcin = '<div id="lochcmr" class="vcard"><h3 class="org">Hamon Custodis M
 var txt_hcal = '<div id="lochcsr" class="vcard"><h3 class="org">Hamon Custodis Southern Region</h3><p class="adr"><span class="tiny hollow button align-right"><a href="https://www.google.com/maps/place/33°34\'52.0"N+86°38\'41.1"W/@33.5811204,-86.6469417,17z">directions</a></span><span class="street-address">941 Alton Parkway</span><br /><span class="locality">Birmingham</span>,&nbsp;<span class="region">Alabama</span>,&nbsp;<span class="postal-code">35210</span><br /><span class="country-name">United States</span></p><p><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:18008432116">1 800-843-2116</a>&nbsp; toll-free<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:12058360057">1 205-836-0057</a><br /><span class="secondary badge"><i class="fi-print"></i></span> 1 205-836-0058</p></div>';
 var txt_hcut = '<div id="lochcwr" class="vcard"><h3 class="org">Hamon Custodis Western Region</h3><p class="adr"><span class="tiny hollow button align-right"><a href="https://www.google.com/maps/place/40°34\'01.7"N+74°36\'35.3"W/@40.5671461,-74.6120047,17z">directions</a></span><span class="street-address">4078 West Nike Drive</span><br /><span class="locality">West Jordan</span>,&nbsp;<span class="region">Utah</span>,&nbsp;<span class="postal-code">84084</span><br /><span class="country-name">United States</span></p><p><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:18006691022">1 800-669-1022</a>&nbsp; toll-free<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:18012558786">1 801-255-8786</a><br /><span class="secondary badge"><i class="fi-print"></i></span> 1 801-255-8796</p></div>';
 
-var txt_corp = '<div id="lochcer" class="vcard"><h3 class="org">Hamon Corporate Headquarters</h3><h4>Hamon Custodis<br />Hamon Custodis Eastern Region<br />Hamon Research-Cottrell<br />Research Cottrell Cooling</h4><p class="adr"><span class="tiny hollow button align-right"><a href="https://www.google.com/maps/place/40°34\'01.7"N+74°36\'35.3"W/@40.5671461,-74.6120047,17z">directions</a></span><span class="street-address">58 East Main Street</span><br /><span class="post-office-box">P.O. Box 1500</span><br /><span class="locality">Somerville</span>,&nbsp;<span class="region">New Jersey</span>,&nbsp;<span class="postal-code">08876</span><br /><span class="country-name">United States</span></p><p><span class="secondary badge"><i class="fi-mail"></i></span> <a class="email" href="mailto:info.hcorp@hamonusa.com">info.hcorp@hamonusa.com</a>&nbsp; toll-free<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:19083332000">1 908-333-2000</a></p></div>';
-var txt_hdi = '<div id="locttc" class="vcard"><h2 class="org">Hamon Deltak</h2><p class="adr"><span class="tiny hollow button align-right"><a href="https://www.google.com/maps/place/44°59\'33.0"N+93°26\'58.3"W/@44.9925038,-93.4517107,17z">directions</a></span><span class="street-address">13330 12th Avenue North</span><br /><span class="locality">Plymouth</span>,&nbsp;<span class="region">Minnesota</span>,&nbsp;<span class="postal-code">55441</span><br /><span class="country-name">United States</span></p><p><span class="secondary badge"><i class="fi-mail"></i></span> <a class="email" href="mailto:info.hd@hamonusa.com">info.hd@hamonusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17635577440">1 763-557-7440</a><br /><span class="secondary badge"><i class="fi-print"></i></span> 1 763-557-4700</p></div>';
-var txt_ttc = '<div id="locttc" class="vcard"><h2 class="org">Thermal Transfer Corporation</h2><p class="adr"><span class="tiny hollow button align-right"><a href="https://www.google.com/maps/place/40°22\'41.6"N+79°50\'53.8"W/@40.3782312,-79.8504684,17z">directions</a></span><span class="street-address">50 North Linden Street</span><br /><span class="locality">Duquesne</span>,&nbsp;<span class="region">Pennsylvania</span>,&nbsp;<span class="postal-code">15110</span><br /><span class="country-name">United States</span></p><p><span class="secondary badge"><i class="fi-mail"></i></span> <a class="email" href="mailto:info.ttc@hamonusa.com">info.ttc@hamonusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:14124604004">1 412-460-4004</a><br /><span class="secondary badge"><i class="fi-print"></i></span> 1 412-466-2899</p></div>';
+// hc offices
+var hcmap = L.map('hcoffices').setView([44.9925,-93.449522],3);
 
+L.tileLayer('https://api.mapbox.com/styles/v1/emijayne/cisaohquy00052yqad6dhqmub/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1pamF5bmUiLCJhIjoiY2lvMWtnbzJqMWFlNnR0bTNxcDhhYW0xaSJ9.tdujy5zALLoojTt2yEGtwA', {
+  attribution: '<strong>HC Office Locations</strong> | &copy; Hamon',
+  accessToken: 'pk.eyJ1IjoiZW1pamF5bmUiLCJhIjoiY2lvMWtnbzJqMWFlNnR0bTNxcDhhYW0xaSJ9.tdujy5zALLoojTt2yEGtwA'
+}).addTo(hcmap);
 
-// main
-var mapmain = L.map('locationmap').setView([44.9925,-93.449522], 4);
-
-L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '<strong>Hamon No.American Office Locations</strong> | &copy Hamon', 
-    accessToken: 'pk.eyJ1IjoiZW1pamF5bmUiLCJhIjoiY2lvMWxrbGxrMWFtaXVrbTNmNWE2dWZkZyJ9.1mt_VncCBhOtJSpSsXN96g'
-  }).addTo(mapmain);
-
-L.marker([40.567142, -74.609816]).addTo(mapmain).bindPopup(txt_corp);
-L.marker([43.856339, -79.387269]).addTo(mapmain).bindPopup(txt_hcca);
-L.marker([39.494215, -87.125675]).addTo(mapmain).bindPopup(txt_hcin);
-L.marker([33.581116, -86.644753]).addTo(mapmain).bindPopup(txt_hcal);
-L.marker([40.601414, -111.988843]).addTo(mapmain).bindPopup(txt_hcut);
-L.marker([44.992500, -93.449522]).addTo(mapmain).bindPopup(txt_hdi);
-L.marker([40.378223, -79.846091]).addTo(mapmain).bindPopup(txt_ttc);
+L.marker([43.856339, -79.387269]).addTo(hcmap).bindPopup(txt_hcca);
+L.marker([40.567142, -74.609816]).addTo(hcmap).bindPopup(txt_hcnj);
+L.marker([39.494215, -87.125675]).addTo(hcmap).bindPopup(txt_hcin);
+L.marker([33.581116, -86.644753]).addTo(hcmap).bindPopup(txt_hcal);
+L.marker([40.601414, -111.988843]).addTo(hcmap).bindPopup(txt_hcut);
