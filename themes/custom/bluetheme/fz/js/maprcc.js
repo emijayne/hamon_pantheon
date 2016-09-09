6a46db70cd896f9bd80e819213b1606f1d74c077
emijayne <emijayne@gmail.com>
2016-09-09 14:45:14 -0400

    rework data for rcc maps

--- themes/custom/bluetheme/fz/js/maprcc.js
+++ themes/custom/bluetheme/fz/js/maprcc.js
@@ -1,10 +1,10 @@
 // text
-var textrepbtu = '<div id="btu" class="vcard"><h3 class="org">BTU Company</h3><h4 class="fn">Erik Heitman</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:erik@btucompany.com">erik@btucompany.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:19139088063" title="mobile phone number">1 913-908-8063</a><br /></p><hr /><p><strong>Territory:</strong> Iowa, Kansas, Missouri, Nebraska</p></div>'
-var textrepahm = '<div id="ahm" class="vcard"><h3 class="org">AHM Associates</h3><h4 class="fn">Lou Brizzolara</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:lbrizzolara@ahmassoc.com">lbrizzolara@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:14152054018" title="mobile phone number">1 415-205-4018</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15107856670" title="office phone number">1 510-785-6670</a>&nbsp;(O)<br /></p><h4 class="fn">Robert Erdman</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:rerdmann@ahmassoc.com">rerdmann@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:12094839964" title="mobile phone number">1 209-483-9964</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15107856670" title="office phone number">1 510-785-6670</a>&nbsp;(O)<br /></p><h4 class="fn">James Harbor</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jharber@ahmassoc.com">jharber@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17148126025" title="mobile phone number">1 714-812-6025</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:19493889571" title="office phone number">1 949-388-9571</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Arizona, California, Nevada</p></div>'
-var textrepast = '<div id="astech" class="vcard"><h3 class="org">As-Tech, Inc.</h3><h4 class="fn">Terry Rush</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:terry.rush@astechusa.com">terry.rush@astechusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:14049318744" title="mobile phone number">1 404-931-8744</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17702051841" title="office phone number">1 770-205-1841</a>&nbsp;(O)<br /></p><h4 class="fn">Chad Nixon</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:chad.nixon@astechusa.com">chad.nixon@astechusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17708621629" title="mobile phone number">1 770-862-1629</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17702051841" title="office phone number">1 770-205-1841</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Alabama, Florida, Georgia, Mississippi, No. Carolina, So. Carolina<br />Excludes Southern Company</p></div>'
-var textreptex = '<div id="txind" class="vcard"><h3 class="org">Texas Industrial Sales</h3><h4 class="fn">Mark Seilkop</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:mark@txindustrialsales.com">mark@txindustrialsales.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17134783689" title="mobile phone number">1 713-478-3689</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:12817414185" title="office phone number">1 281-741-4185</a><br /></p><hr /><p><strong>Territory:</strong> Texas<br /><em>new construction only</em></p></div>'
-var textrepapa = '<div id="apa" class="vcard"><h3 class="org">APA Inc.</h3><h4 class="fn">Jene Bramel</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jbramelapa@fuse.net">jbramelapa@fuse.net</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15134846315" title="mobile phone number">1 513-484-6315</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15135610687" title="office phone number">1 513-561-0687</a>&nbsp;(O)<br /></p><h4 class="fn">Doug Cron</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:dcronapa@fuse.net">dcronapa@fuse.net</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15136040396" title="mobile phone number">1 513-604-0396</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15135610687" title="office phone number">1 513-561-0687</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Southern Illinois, Indiana, Ohio, Western Pennsylvania, Kentucky, West Virginia</p></div>'
-var textrepcej = '<div id="cejka" class="vcard"><h3 class="org">Cejka Industrial </h3><h4 class="fn">John Cejka</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jcejka@cejkafixindustrial.com">jcejka@cejkafixindustrial.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:18329043000" title="mobile phone number">1 832-904-3000</a><br /></p><hr /><p><strong>Territory:</strong> Texas<br /><em>aftermarket only</em></p></div>'
+var textrccbtu = '<div id="btu" class="vcard"><h3 class="org">BTU Company</h3><h4 class="fn">Erik Heitman</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:erik@btucompany.com">erik@btucompany.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:19139088063" title="mobile phone number">1 913-908-8063</a><br /></p><hr /><p><strong>Territory:</strong> Iowa, Kansas, Missouri, Nebraska</p></div>'
+var textrccahm = '<div id="ahm" class="vcard"><h3 class="org">AHM Associates</h3><h4 class="fn">Lou Brizzolara</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:lbrizzolara@ahmassoc.com">lbrizzolara@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:14152054018" title="mobile phone number">1 415-205-4018</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15107856670" title="office phone number">1 510-785-6670</a>&nbsp;(O)<br /></p><h4 class="fn">Robert Erdman</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:rerdmann@ahmassoc.com">rerdmann@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:12094839964" title="mobile phone number">1 209-483-9964</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15107856670" title="office phone number">1 510-785-6670</a>&nbsp;(O)<br /></p><h4 class="fn">James Harbor</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jharber@ahmassoc.com">jharber@ahmassoc.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17148126025" title="mobile phone number">1 714-812-6025</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:19493889571" title="office phone number">1 949-388-9571</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Arizona, California, Nevada</p></div>'
+var textrccast = '<div id="astech" class="vcard"><h3 class="org">As-Tech, Inc.</h3><h4 class="fn">Terry Rush</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:terry.rush@astechusa.com">terry.rush@astechusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:14049318744" title="mobile phone number">1 404-931-8744</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17702051841" title="office phone number">1 770-205-1841</a>&nbsp;(O)<br /></p><h4 class="fn">Chad Nixon</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:chad.nixon@astechusa.com">chad.nixon@astechusa.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17708621629" title="mobile phone number">1 770-862-1629</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17702051841" title="office phone number">1 770-205-1841</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Alabama, Florida, Georgia, Mississippi, No. Carolina, So. Carolina<br />Excludes Southern Company</p></div>'
+var textrcctex = '<div id="txind" class="vcard"><h3 class="org">Texas Industrial Sales</h3><h4 class="fn">Mark Seilkop</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:mark@txindustrialsales.com">mark@txindustrialsales.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:17134783689" title="mobile phone number">1 713-478-3689</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:12817414185" title="office phone number">1 281-741-4185</a><br /></p><hr /><p><strong>Territory:</strong> Texas<br /><em>new construction only</em></p></div>'
+var textrccapa = '<div id="apa" class="vcard"><h3 class="org">APA Inc.</h3><h4 class="fn">Jene Bramel</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jbramelapa@fuse.net">jbramelapa@fuse.net</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15134846315" title="mobile phone number">1 513-484-6315</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15135610687" title="office phone number">1 513-561-0687</a>&nbsp;(O)<br /></p><h4 class="fn">Doug Cron</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:dcronapa@fuse.net">dcronapa@fuse.net</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15136040396" title="mobile phone number">1 513-604-0396</a>&nbsp;(M)<br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:15135610687" title="office phone number">1 513-561-0687</a>&nbsp;(O)<br /></p><hr /><p><strong>Territory:</strong> Southern Illinois, Indiana, Ohio, Western Pennsylvania, Kentucky, West Virginia</p></div>'
+var textrcccej = '<div id="cejka" class="vcard"><h3 class="org">Cejka Industrial </h3><h4 class="fn">John Cejka</h4><p><span class="secondary badge"><i class="fi-mail"></i></span> <a href="mailto:jcejka@cejkafixindustrial.com">jcejka@cejkafixindustrial.com</a><br /><span class="secondary badge"><i class="fi-telephone"></i></span> <a class="tel" href="tel:18329043000" title="mobile phone number">1 832-904-3000</a><br /></p><hr /><p><strong>Territory:</strong> Texas<br /><em>aftermarket only</em></p></div>'
 
 
 // reps rcc
@@ -16,47 +16,47 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
   }).addTo(mapmain);
 
 
-L.geoJson([newcon, afterm], {
+L.geoJson([rcc_oem, afterm], {
   style: function (feature) {
     switch(feature.properties.REP_ID) {
-      case 'textrepbtu':
+      case 'rcc_btu':
         return {color: "#af8dc3"};
         break;
-      case 'textrepahm':
+      case 'rcc_ahm':
         return {color: "#e7d4e8"};
         break;
-      case 'textrepast':
+      case 'rcc_ast':
         return {color: "#1b7837"};
         break;
-      case 'textreptex':
+      case 'rcc_tex':
         return {color: "#762a83"};
         break;
-      case 'textrepapa':
+      case 'rcc_apa':
         return {color: "#7fbf7b"};
         break;
-      case 'textrepcej':
+      case 'rcc_cej':
         return {color: "#d9f0d3"};
     }
   },
   onEachFeature: function (feature, layer) {
     switch(feature.properties.REP_ID) {
-      case 'textrepbtu':
-        layer.bindPopup(textrepbtu);
+      case 'rcc_btu':
+        layer.bindPopup(textrccbtu);
         break;
-      case 'textrepahm':
-        layer.bindPopup(textrepahm);
+      case 'rcc_ahm':
+        layer.bindPopup(textrccahm);
         break;
-      case 'textrepast':
-        layer.bindPopup(textrepast);
+      case 'rcc_ast':
+        layer.bindPopup(textrccast);
         break;
-      case 'textreptex':
-        layer.bindPopup(textreptex);
+      case 'rcc_tex':
+        layer.bindPopup(textrcctex);
         break;
-      case 'textrepapa':
-        layer.bindPopup(textrepapa);
+      case 'rcc_apa':
+        layer.bindPopup(textrccapa);
         break;
-      case 'textrepcej':
-        layer.bindPopup(textrepcej);
+      case 'rcc_cej':
+        layer.bindPopup(textrcccej);
         break;
       default:
         layer.bindPopup(feature.properties.popupContent);
@@ -65,12 +65,12 @@ L.geoJson([newcon, afterm], {
 });
 
 
-var newcon = [datarepbtu, datarepahm, datarepast, datareptex, datarepapa],
-    afterm = [datarepbtu, datarepahm, datarepast, datarepcej, datarepapa];
+var rcc_oem = [rcc_btu, rcc_ahm, rcc_ast, rcc_tex, rcc_apa],
+    rcc_aft = [rcc_btu, rcc_ahm, rcc_ast, rcc_cej, rcc_apa];
 
 var overlays = {
-  "New Construction": newcon, 
-  "Aftermarket": afterm
+  "New Construction": rcc_oem, 
+  "Aftermarket": rcc_aft
 };
 
 L.control.layers(overlays).addTo(maprcc);
